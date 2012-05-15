2245992a85f34bde2438938a3b485de03ca3a3d7
Zhiwei YAN <jerod.yan@gmail.com>
2012-05-16 00:34:19 +0800

    second commit

--- msclean.bat
+++ msclean.bat
@@ -1,3 +1,7 @@
-del /F /S *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps 
+del /F /S *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps *.lol
 
 del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi
+
+del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi .DS_Store
+del /S /F *.brf *.nls *.nlo *.synctex.gz *.synctex.gz(busy)
+del /S /F ._*.*
