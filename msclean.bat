7504de23e41104b303b3b7260f1b5e3c0cf50050
Zhiwei YAN <jerod.yan@gmail.com>
2012-06-14 00:51:18 +0800

    a little slow, need fast

--- msclean.bat
+++ msclean.bat
@@ -1,6 +1,6 @@
 del /F /S *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps *.lol
 
-del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi
+del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi *.toe *.xdv 
 
 del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi .DS_Store
 del /S /F *.brf *.nls *.nlo *.synctex.gz *.synctex.gz(busy)
