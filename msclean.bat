bb00a4a43bf64029f3af119a40651241effebb75
Zhiwei YAN <jerod.yan@gmail.com>
2013-01-02 21:43:19 +0800

    Not delete *.idx

--- msclean.bat
+++ msclean.bat
@@ -1,4 +1,4 @@
-del /F /S *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps *.lol
+del /F /S *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps *.lol
 
 del /S /F *.aux *.bbl *.blg *.log *.bak *.dvi *.toe *.xdv 
 
